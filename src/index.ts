2dd978b61ad7a23227182baf8f6c1fee11907c6e
Adrián Scott <adrian.lga@hotmail.com>
2021-11-13 19:09:36 -0700

    index file

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as maskCreditCard } from './maskCreditCard';
+export { default as numberToOrdinal } from './numberToOrdinal';
+export { default as calculateRPN } from './calculateRPN';
