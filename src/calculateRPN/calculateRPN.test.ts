c4bf7045bb925fbbe0508f7a61438a8e69275d5a
Adrián Scott <adrian.lga@hotmail.com>
2021-11-13 19:09:23 -0700

    calculateRPN tests

---
+++ src/calculateRPN/calculateRPN.test.ts
@@ -0,0 +1,15 @@
+import calculateRPN from "./index";
+
+describe("RPN calculation", () => {
+  it("Calculation", () => {
+    expect(calculateRPN("3")).toBe(3);
+    expect(calculateRPN("3 4 +")).toBe(7);
+    expect(calculateRPN("2 9 5 + -")).toBe(12);
+    expect(calculateRPN("2 9 5 + -")).toBe(12);
+  });
+
+  it("Test invalid inputs", () => {
+    expect(()=>calculateRPN("5+")).toThrow("5+ is not supported");
+    expect(()=>calculateRPN("3 + +")).toThrow("Invalid expression");
+  })
+})
